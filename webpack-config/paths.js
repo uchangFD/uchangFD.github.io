c5279533993e29f4bd7c5b90499365a1c0fcba94
uchang.fd <uchang.fd@gmail.com>
2019-02-07 21:10:30 +0900

    scaffolding

--- webpack-config/paths.js
+++ webpack-config/paths.js
@@ -2,7 +2,7 @@ import path from "path";
 
 module.exports = {
   root: path.resolve(__dirname, "../"),
-  outputPath: path.resolve(__dirname, "../", "dist"),
+  outputPath: path.resolve(__dirname, "../", "docs"),
   entryPath: path.resolve(__dirname, "../", "src/index.js"),
   templatePath: path.resolve(__dirname, "../", "src/index.html"),
   imagesFolder: "assets/img",
--- webpack-config/rules.js
+++ webpack-config/rules.js
@@ -23,25 +23,25 @@ module.exports = [
       },
     ],
   },
-  // {
-  //   test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-  //   exclude: /node_modules/,
-  //   use: {
-  //     loader: "file-loader",
-  //   },
-  // },
-  // {
-  //   test: /\.(woff|woff2)$/,
-  //   exclude: /node_modules/,
-  //   loader: "url-loader?prefix=font/&limit=5000",
-  // },
-  // {
-  //   test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-  //   exclude: /node_modules/,
-  //   loader: "url-loader?limit=10000&mimetype=application/octet-stream",
-  // },
-  // {
-  //   test: /\.(jpe?g|png|gif|svg)$/i,
-  //   use: ["url-loader?limit=10000", "img-loader"],
-  // },
+  {
+    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "file-loader",
+    },
+  },
+  {
+    test: /\.(woff|woff2)$/,
+    exclude: /node_modules/,
+    loader: "url-loader?prefix=font/&limit=5000",
+  },
+  {
+    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+    exclude: /node_modules/,
+    loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+  },
+  {
+    test: /\.(jpe?g|png|gif|svg)$/i,
+    use: ["url-loader?limit=10000", "img-loader"],
+  },
 ];
